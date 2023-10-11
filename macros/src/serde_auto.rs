26c2aecdaeff4b61a57e1080abccb9ccd887839b
Thipok Ch <thipok.ch@gmail.com>
2023-10-11 13:34:54 +0700

    feat: serde_auto default for DataFrame

--- macros/src/serde_auto.rs
+++ macros/src/serde_auto.rs
@@ -7,6 +7,7 @@ pub fn expand_serde_auto(item: TokenStream) -> syn::Result<TokenStream> {
         #[serde_with::apply(
             Option => #[serde(default, skip_serializing_if = "Option::is_none")],
             Vec => #[serde(default, skip_serializing_if = "Vec::is_empty")],
+            DataFrame => #[serde(default, skip_serializing_if = "Vec::is_empty")],
         )]
         #item
     };
